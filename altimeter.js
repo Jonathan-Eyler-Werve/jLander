c7f5653a82d0131427ed55d8c3858b5d7cad8ce7
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2013-12-31 11:56:39 -0600

    Broke everything. Altimeter WIP.

--- altimeter.js
+++ altimeter.js
@@ -2,30 +2,41 @@
 // Altimeter.js 
 
 var altimeterBase = new image();
-var altimeterThousands = new image();
+
+var altimeterThousandsBase = new image();
+var altimeterThousandsTop = new image();
 var altimeterHundreds = new image();
 var altimeterTens = new image();
 var altimeterText = new image();
 
+function altimeterInit(){
+  altimeterBase.src = "/source-images/dials-base.svg";
+  altimeterThousandsBase.src = "/source-images/dials-10k-bottom.svg";
+  altimeterThousandsTop.src = "/source-images/dials-10k-top.svg";
+  altimeterHundreds.src = "/source-images/dials-100.svg";
+  altimeterTens.src = "/source-images/dials-100.svg";
+  altimeterText.src = "/source-images/dials-text.svg";
+}
+
 function drawAltimeter(ctx, number){
 
+ console.log("drawAltimeter called")
 	ctx.save();
-  ctx.translate(50, 50);
+  ctx.translate(10, 50);
 
-	drawBase(ctx, number);
-	drawThousands(ctx, number);
-	drawHundreds(ctx, number);
-	drawTens(ctx, number);
-	drawText(ctx, number);
+	drawBase(dialContext, number);
+	// drawThousands(ctx, number);
+	// drawHundreds(ctx, number);
+	// drawTens(ctx, number);
+	// drawText(ctx, number);
 
   ctx.restore();
 
 };
 
-function drawBase(){
-	ctx.drawImage(lander,-32,-32);
+function drawBase(_ctx, number){
+	_ctx.drawImage(altimeterBase,0,0);
 };
-
 
 
 
