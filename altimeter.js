b33dc1ed813d0c3936045693fdd2ba5d72f47651
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2013-12-30 23:56:36 -0600

    nonworking - drawData work in progress to add altimeter

---
+++ altimeter.js
@@ -0,0 +1,19 @@
+
+// Altimeter.js 
+
+function drawData(ctx){
+	drawBase();
+	drawThousands(number);
+	drawHundreds(number);
+	drawTens(number);
+	drawText();
+};
+
+function drawBase(){
+
+};
+
+drawData(myContext);
+
+
+
