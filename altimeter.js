a3444c281c8632a44d17efa1c9519573c84d9358
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2013-12-31 16:54:34 -0600

    Altimeter working

--- altimeter.js
+++ altimeter.js
@@ -15,9 +15,9 @@ function altimeterInit(){
   altimeterThousandsBase.src = "source-images/dials-10k-bottom.svg";
   altimeterThousandsTop.src = "source-images/dials-10k-top.svg";
   altimeterHundreds.src = "source-images/dials-100.svg";
-  altimeterTens.src = "source-images/dials-100.svg";
+  altimeterTens.src = "source-images/dials-10.svg";
   altimeterText.src = "source-images/dials-text.svg";
-}
+};
 
 function drawAltimeter(ctx, number){
 
@@ -26,11 +26,10 @@ function drawAltimeter(ctx, number){
   ctx.translate(50,50);
 
 	drawBase(ctx, number);
-
-	drawThousands(ctx, altitude());
-	// drawHundreds(ctx, number);
-	// drawTens(ctx, number);
+	drawThousands(ctx, number);
 	drawText(ctx, number);
+	drawHundreds(ctx, number);
+	drawTens(ctx, number);
 
   ctx.restore();
 
@@ -43,10 +42,28 @@ function drawBase(_ctx, number){
 
 
 function drawThousands(_ctx, number) {
+	console.log(number);
 	_ctx.save();
-	_ctx.translate(95,90);
-	_ctx.rotate(to_rad(pitch));
-	_ctx.drawImage(altimeterThousandsBase,-95,-90)
+	_ctx.translate(90,90);
+	_ctx.rotate(to_rad(number/(10000/360)));
+	_ctx.drawImage(altimeterThousandsBase,-90,-90);
+	_ctx.restore();
+	_ctx.drawImage(altimeterThousandsTop,0,0);
+};
+
+function drawHundreds(_ctx, number) {
+	_ctx.save();
+	_ctx.translate(90,90);
+	_ctx.rotate(to_rad(number/(1000/360)));
+	_ctx.drawImage(altimeterHundreds,-90,-90);
+	_ctx.restore();
+};
+
+function drawTens(_ctx, number) {
+	_ctx.save();
+	_ctx.translate(90,90);
+	_ctx.rotate(to_rad(number/(100/360)));
+	_ctx.drawImage(altimeterTens,-90,-90);
 	_ctx.restore();
 };
 
