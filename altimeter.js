f959c1bfc8ac0628fc9547e878229a011fb5e333
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2013-12-31 11:07:06 -0600

    Smaller lander WIP

--- altimeter.js
+++ altimeter.js
@@ -1,19 +1,36 @@
 
 // Altimeter.js 
 
-function drawData(ctx){
-	drawBase();
-	drawThousands(number);
-	drawHundreds(number);
-	drawTens(number);
-	drawText();
+var altimeterBase = new image();
+var altimeterThousands = new image();
+var altimeterHundreds = new image();
+var altimeterTens = new image();
+var altimeterText = new image();
+
+function drawAltimeter(ctx, number){
+
+	ctx.save();
+  ctx.translate(50, 50);
+
+	drawBase(ctx, number);
+	drawThousands(ctx, number);
+	drawHundreds(ctx, number);
+	drawTens(ctx, number);
+	drawText(ctx, number);
+
+  ctx.restore();
+
 };
 
 function drawBase(){
-
+	ctx.drawImage(lander,-32,-32);
 };
 
-drawData(myContext);
 
 
 
+
+
+///////////   
+
+ctx.rotate(to_rad(pitch));
