a46a60903fe1aaf41420348ba2b16c120d1794bf
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-01-02 01:06:02 -0600

    Fuel initialized

--- fuel.js
+++ fuel.js
@@ -1,8 +1,27 @@
 // fuel.js
 
+var fuelBase = new Image();
+var fuelLampDoNot = new Image();
+var fuelLampPanic = new Image();
+var fuelArrow = new Image();
+var fuelOverlay = new Image();
+
+function fuelInit(){
+
+	console.log("fuelInit runs");
+  fuelBase.src = "source-images/fuel-base.svg";
+  fuelLampDoNot.src = "source-images/fuel-lamp1.svg";
+  fuelLampPanic.src = "source-images/fuel-lamp2.svg";
+  fuelArrow.src = "source-images/fuel-arrow.svg";
+  fuelOverlay.src = "source-images/fuel-overlay.svg";
+};
+
+
 
 function consumeFuel (_thrust) {
   if (_thrust > 0) {
     fuel += -1; 
   };
 };
+
+fuelInit();
